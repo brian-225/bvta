152dae1f685d18825d80876c35304dfc9d9aa33c
Brian <106784130+brian-225@users.noreply.github.com>
2022-09-26 20:00:12 -0700

    Add files via upload
    commited-snakeUpdate

---
+++ snake-game/scratch.js
@@ -0,0 +1,35 @@
+/*
+scratch.js
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelectorAll(".grid div") 
+    consg scoreDisplay = document.querySelector("spam")
+    const startButton = document.querySelector(".start")
+
+    const width = 10
+    let currentIndex = 0
+    let juiceIndex = 0
+    let currentSnake = [2,1,0]
+
+    let direction = 1
+    let score = 0
+    let speed = 0
+    let intervalTime = 0
+    let interval = 0
+
+    Function AbortController(e){
+        squares[currentIndex].classList.remove("snake")
+
+        if(e.keyCode === 39) {
+            direction = 1
+        }   else if(e.keyCode === 38)
+            direction = -width
+        }   else if(e.keyCode === 37)
+            direction -1
+        }   else if(e.keyCode === 40)
+            direction = +width
+        } 
+
+})
+
+
+*/
